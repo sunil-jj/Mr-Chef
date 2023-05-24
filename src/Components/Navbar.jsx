dc2e7c9c32be8a4cf566e3af0764adf4649cf79b
Sunil-JJ <sunilamaranth@gmail.com>
2023-05-24 16:03:17 +0530

    a

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     <Box bg="white" h="80px">
         This is Box
       <Image marginLeft="20px" marginTop="-15px" boxSize="70px" width="100px" src="https://scontent-bom1-2.xx.fbcdn.net/v/t39.30808-6/292303680_354333216847986_200622443922396658_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Ub5zuRLiuvsAX-XZ6po&_nc_ht=scontent-bom1-2.xx&oh=00_AfCkgj7eM0OlvcZShefDt6cLXFNyLAfZ4utEXKxFsQ9Sbg&oe=64723EDA "/>
-        <h2>Street Food</h2>
+        <h2 style={{marginRight:"90%",fontFamily:"sans-serif",fontWeight:"400px",fontSize:"15px"}}>Street food</h2>
     </Box>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/provider';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+<ChakraProvider>
   <App />
+
+</ChakraProvider>
 
   
 );
