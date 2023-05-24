ec29a0498d00b376bf9894956e00c6a3e898b5ba
Sunil-JJ <sunilamaranth@gmail.com>
2023-05-24 14:36:22 +0530

    nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Box, Image} from '@chakra-ui/react'
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <Box bg="white" h="80px">
+        This is Box
+      <Image marginLeft="20px" marginTop="-15px" boxSize="70px" width="100px" src="https://scontent-bom1-2.xx.fbcdn.net/v/t39.30808-6/292303680_354333216847986_200622443922396658_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Ub5zuRLiuvsAX-XZ6po&_nc_ht=scontent-bom1-2.xx&oh=00_AfCkgj7eM0OlvcZShefDt6cLXFNyLAfZ4utEXKxFsQ9Sbg&oe=64723EDA "/>
+    </Box>
   )
 }
 
