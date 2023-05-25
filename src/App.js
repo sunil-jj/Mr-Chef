b32acda025b3682f72726e3b231b26519df8e5f4
RahulDobariya <sendydobariya007@gmail.com>
2023-05-25 18:32:03 +0530

    feat: Cart Page UI done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import YourOrder from './Components/YourOrder';
 
 function App() {
   return (
     <div className="App">
-      
+      <YourOrder />
     </div>
   );
 }
