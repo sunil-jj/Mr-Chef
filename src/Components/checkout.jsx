c90760ea66a0b1f8365c92010186ee1cfe5fb326
your_name_here <arbazshaikh221297@gmail.com>
2023-05-26 22:30:19 +0530

    cechout r2

--- src/Components/checkout.jsx
+++ src/Components/checkout.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import { Input,Box,Center,Text,Checkbox,Flex,Spacer,Stack,Textarea} from '@chakra-ui/react'
+import { Input,Box,Center,Text,Checkbox,Flex,Spacer,Stack,Textarea, Button,Image} from '@chakra-ui/react'
 import styles from "./checkout.module.css";
+import { useState } from 'react';
+import Otp from './otp';
 
 function Checkout() {
+  const [creditCard,setcreditcard]=useState({state:"none",
+ cardNo:"",
+cvv:"",
+date:""});
+
+
+  let handleClick=()=>{
+    if(creditCard.cardNo==""||creditCard.date==""||creditCard.cvv==""){
+      alert('fill details')
+ 
+    }else{
+     return(
+      <Box>
+        <Button>hiohlb</Button>
+      </Box>
+     )       
+  
+    }
+  }
+
+ 
+ 
   return (
-    <div className='lll'>
+    
+      <Box>
       <Center>
-      <Box w="30%" height="600px" align="left" padding="15px" borderWidth='2px' borderRadius='lg' overflow='hidden'>
+      <Stack>
+      <Box w="500px" height="600px" align="left" padding="15px" borderWidth='2px'backgroundColor="white" borderRadius='lg' overflow='hidden'>
        <Text fontSize="18px">DELIVERY TERMS</Text>
        <Checkbox size ="md" iconColor="black" borderColor="black" colorScheme="yellow">Delivery</Checkbox><br />
        <Checkbox size ="md" iconColor="black" borderColor="black" colorScheme="yellow">Pickup</Checkbox><br/><br/>     
@@ -30,19 +56,84 @@ function Checkout() {
        
        </Box>
        <Text fontSize="18px">TERMS OF PAYMENT</Text>
-       <div className="carddiv" >
-        <img className={styles.creditCard}  src="https://cutewallpaper.org/24/credit-card-icon-png/card-clipart-payment-card-credit-card-icon-png-white-transparent-png-226663-pinclipart.png" ></img>
-        </div>
+       
+       <Button height = "60px" colorScheme='red'  variant='ghost'>
+       <Center>
+       <Stack>
+        <img width="50px" className={styles.creditCard}  src="https://cutewallpaper.org/24/credit-card-icon-png/card-clipart-payment-card-credit-card-icon-png-white-transparent-png-226663-pinclipart.png" ></img>
+        <Text color="black" fontSize="11px">Credit Card</Text>
+        </Stack>
+        </Center>
+        </Button>
+       
+        
+       
+        <Button height = "60px"colorScheme='red' variant='ghost' onClick={()=>{creditCard=="none"? setcreditcard("block"):setcreditcard("none")}}>
+        <Center>
+          <Stack>
+           
+          <img width="30px"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTetjh5NS8Lq3poSJB9aeTHUVW-jVpUa-LMadaQvxqv8w&usqp=CAU&ec=48600113" ></img>
+          <Text color="black" fontSize="11px">Cash</Text>
+          
+          </Stack>
+          </Center>
+       
+        </Button>
+       
        <Box>
 
        </Box>
        </Box>
-      </Center>
+
+       <Box display={creditCard} width="500px" >
+        
+          <Stack>
+            
+          <Text>Please fill your card details below</Text>
+          <Box padding="20px" borderRadius="15px" color ="white" bgImage="https://thumbs.dreamstime.com/b/world-map-black-background-6642186.jpg" backgroundSize="100%">
+          <Image margin="40px 0 0 20px" w="50px"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRidJ0AVVVWt5nQXOkx-Npbx0Lj-L4kpx4T6XTpEcZuNQ&usqp=CAU&ec=48600113" borderRadius="10px" borderBottomRadius="15px"/>
+          <Text marginTop="35px" align="left">CARD NO.</Text>
+          <Input  fontSize="20px" width="100%" varient="flushed" type="number" placeholder=" XXXX XXXX XXXX XXXX" onChange={(e)=>setcreditcard({...creditCard,cardNo:e.target.value})}/><br /><br />
+          
+          <Flex>
+            <Stack alignItems="start" >
+            <Text>VALID THRU</Text>
+          <Input width="50%" varient="date"   placeholder='05/23' onChange={(e)=>setcreditcard({...creditCard,date:e.target.value})}/>
+            </Stack>
+            <Stack alignItems="start" >
+            <Text>CVV</Text>
+          <Input width="40%" type="number" placeholder="000" onChange={(e)=>setcreditcard({...creditCard,cvv:e.target.value})}/>
+          </Stack>
+          <img width="100px"  src="https://th.bing.com/th/id/OIP.RJ31bOdHwatvOYjWMG5EywHaEr?w=237&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7" alt="" />
+      
+           
+
+          </Flex>
+          
+          </Box>
+          
+          
+          </Stack>
+        
+        
+        
+      </Box>
+
+      <Button colorScheme='teal' varient="outline" onClick={handleClick}>Make Payment</Button>
+      </Stack>
+      </Center>  
+
+      
+          
        
    
 
-    </div>
+      </Box>
   )
 }
+
+
+
+
 
 export default Checkout
---
+++ src/Components/otp.jsx
@@ -0,0 +1,11 @@
+const Otp=()=>{
+
+
+    return <>
+    <h1>hi</h1>
+
+
+    </>
+}
+
+export default Otp
