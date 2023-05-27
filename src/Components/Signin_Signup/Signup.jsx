d3b497f4ba0e0822d61c8b0506cf38caa32959bd
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2023-05-27 16:15:52 +0530

    finished

--- src/Components/Signin_Signup/Signup.jsx
+++ src/Components/Signin_Signup/Signup.jsx
@@ -14,35 +14,52 @@ import {
   InputRightElement,
   Button,
   Checkbox,
-  Text
+  Icon,
+  useToast
 } from "@chakra-ui/react";
 import { FaUserAlt } from "react-icons/fa";
 import { BsFillTelephonePlusFill } from "react-icons/bs";
-import {RiLockPasswordFill} from "react-icons/ri"
+import { RiLockPasswordFill } from "react-icons/ri";
 import { MdEmail } from "react-icons/md";
 import styles from "./style.module.css";
 import React from "react";
-import background from "./bg2.jpg";
+import background from "../Signin_Signup/images/bg2.jpg";
+import Signin from "./Signin";
 import { useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
+import Loader from "./Loader";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import axios from "axios"
+import {useDispatch} from "react-redux"
+import { postUserSuccess, postUserFaliure } from "../../Redux/actionCreator";
+
 
 function Signup(props) {
+  const dispatch = useDispatch();
   const [inputFocus, setInputFocus] = useState({
     field1: false,
     field2: false,
     field3: false,
     field4: false,
   });
+  const toast = useToast()
 
-  const [show, setShow] = useState(false)
-  const [captcha, setcaptcha] = useState(false); 
+  const [show, setShow] = useState(false);
+  const [captcha, setcaptcha] = useState(false);
+  const [loader, setloader] = useState(false);
 
+  const [isChecked, setisChecked] = useState(false);
 
-  const handleClick = () => setShow(!show)
+  const [name, setName] = useState("");
+  const [mobile, setmobile] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClick = () => setShow(!show);
 
   const onChange = () => {
     // captcha
-    setcaptcha(true);
+    setcaptcha(!captcha);
   };
 
   const handleInputFocus = (fieldName) => {
@@ -57,6 +74,85 @@ function Signup(props) {
       ...prevState,
       [fieldName]: false,
     }));
+  };
+
+  const handleLoader = () => {
+    
+  };
+
+  //validation
+
+  const isValidated = () => {
+    let isproceed = true;
+    let errormessage = "";
+    if (name === null || name === "") {
+      isproceed = false;
+      errormessage += " Name";
+    }
+    if (mobile === null || mobile === "") {
+      isproceed = false;
+      errormessage += " Country";
+    }
+    if (email === null || email === "") {
+      isproceed = false;
+      errormessage += " Email";
+    }else if(!email.includes("@")){
+      isproceed = false;
+      errormessage += " Invalid Email";
+    }
+    if (password === null || password === "") {
+      isproceed = false;
+      errormessage += " Password";
+    }
+    
+
+    if (!isproceed) {
+      toast({
+        title: "Enter "+errormessage,
+        description: "Registration failed due to :" + errormessage,
+        status: "warning",
+        duration: 2000,
+        isClosable: true,
+      });
+      // toast.warning(errormessage);
+    } else {
+      //write something
+    }
+    return isproceed;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let userDataObj = { name, mobile, email, password };
+    if (isValidated()) {
+      axios.post("http://localhost:8080/users", userDataObj)
+        .then((res) => {
+          dispatch(postUserSuccess(res.data));
+          toast({
+            title: "Registration successfully",
+            description: "Account created successfully",
+            status: "success",
+            duration: 2000,
+            isClosable: true,
+          });
+          setloader(true);
+    setTimeout(() => {
+      setloader(false);
+    }, 5000);
+          // navigate("/signin");
+        })
+        .catch((err) => {
+          dispatch(postUserFaliure())
+          toast({
+            title: "Registration Failed",
+            description: "Failed :" + err.message,
+            status: "error",
+            duration: 2000,
+            isClosable: true,
+          });
+          // toast.error("Failed :" + err.message);
+        });
+    }
   };
 
   return (
@@ -86,8 +182,8 @@ function Signup(props) {
               mt="-3px"
             >
               <Center>
-                <Box className={styles.welcome_box}>
-                  <Heading pt="10px" as="h1" className={styles.welcome_box}>
+                <Box className={styles.welcome_div}>
+                  <Heading pt="10px" as="h1" className={styles.welcome_div}>
                     Welcome
                   </Heading>
                 </Box>
@@ -105,7 +201,8 @@ function Signup(props) {
 
                   <TabPanels>
                     <TabPanel>
-                      <p>one!</p>
+                      {/*  */}
+                      <Signin />
                     </TabPanel>
                     <TabPanel>
                       <Stack spacing={4}>
@@ -117,6 +214,8 @@ function Signup(props) {
                             <FaUserAlt />
                           </InputLeftElement>
                           <Input
+                          onChange={(e) => setName(e.target.value)}
+                          value={name}
                             variant="flushed"
                             type="text"
                             placeholder="Full Name"
@@ -135,8 +234,10 @@ function Signup(props) {
                             <BsFillTelephonePlusFill />
                           </InputLeftElement>
                           <Input
+                          onChange={(e) => setmobile(e.target.value)}
+                          value={mobile}
                             variant="flushed"
-                            type="tel"
+                            type="number"
                             placeholder="Phone number"
                             focusBorderColor="#B7791F"
                             borderColor="#011029"
@@ -153,6 +254,8 @@ function Signup(props) {
                             <MdEmail />
                           </InputLeftElement>
                           <Input
+                          onChange={(e) => setemail(e.target.value)}
+                          value={email}
                             variant="flushed"
                             type="email"
                             placeholder="Email Address"
@@ -171,36 +274,66 @@ function Signup(props) {
                             <RiLockPasswordFill />
                           </InputLeftElement>
                           <Input
+                          onChange={(e) => setPassword(e.target.value)}
+                          value={password}
                             variant="flushed"
-                            pr='4.5rem'
-        type={show ? 'text' : 'password'}
+                            pr="4.5rem"
+                            type={show ? "text" : "password"}
                             placeholder="Password"
                             focusBorderColor="#B7791F"
                             borderColor="#011029"
                             onFocus={() => handleInputFocus("field4")}
                             onBlur={() => handleInputBlur("field4")}
                           />
-                          <InputRightElement width='4.5rem'>
-                          <Button h='1.75rem' size='sm' onClick={handleClick}>
-          {show ? 'Hide' : 'Show'}
-        </Button>
+                          <InputRightElement width="4.5rem">
+                            <Button h="1.75rem" size="sm" onClick={handleClick}>
+                              {show ? (
+                                <Icon boxSize={4} as={FiEye}></Icon>
+                              ) : (
+                                <Icon boxSize={4} as={FiEyeOff}></Icon>
+                              )}
+                            </Button>
                           </InputRightElement>
                         </InputGroup>
                       </Stack>
                       <Center py={"20px"}>
-              <Checkbox colorScheme='green' pr={"7px"} /> <p style={{ fontSize:"15px"}}>I accept<span style={{ fontWeight:"bold", color:"green"}}> Terms of Use</span></p>
-              </Center>
+                        <Checkbox onChange={()=>setisChecked(!isChecked)} colorScheme="green" pr={"7px"} />{" "}
+                        <p style={{ fontSize: "15px" }}>
+                          I accept
+                          <span style={{ fontWeight: "bold", color: "green" }}>
+                            Terms of Use
+                          </span>
+                        </p>
+                      </Center>
                       <Center>
-                {/* google captcha */}
-                <ReCAPTCHA
-                  sitekey="6LdecqQlAAAAAF5O-JC8ProsSC_nHykNvfTpWp2B"
-                  onChange={onChange}
-                />
-              </Center>
-              <Center py={"20px"}>
-                <Button colorScheme='orange'>Register Now</Button>
-              </Center>
-              <p className={styles.textAnimation} ><span style={{color:"#DD6A1F",fontWeight:"bold", fontSize:"30px"}}>-5%</span> on everything for registered users</p>
+                        {/* google captcha */}
+                        <ReCAPTCHA
+                          sitekey="6LdecqQlAAAAAF5O-JC8ProsSC_nHykNvfTpWp2B"
+                          onChange={onChange}
+                        />
+                      </Center>
+                      <Center py={"20px"}>
+                        {
+                          // handleLoader
+                        isChecked && captcha ? <Button onClick={handleSubmit} colorScheme="orange">
+                         {loader ? <Loader /> : "Register Now"}
+                       </Button> : <Button isDisabled  colorScheme="orange">
+                            {loader ? <Loader /> : "Register Now"}
+                          </Button>
+                        }
+                      </Center>
+                      <p className={styles.textAnimation}>
+                        <span
+                          style={{
+                            color: "#DD6A1F",
+                            fontWeight: "bold",
+                            fontSize: "30px",
+                          }}
+                        >
+                          -5%
+                        </span>{" "}
+                        on everything for registered users
+                      </p>
                     </TabPanel>
                   </TabPanels>
                 </Tabs>
@@ -213,4 +346,4 @@ function Signup(props) {
   );
 }
 
-export {Signup};
+export { Signup };
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -4,4 +4,9 @@
 import { legacy_createStore } from "redux";
 import { reducer } from "./reducer";
 
+
 export const store=legacy_createStore(reducer);
+
+store.subscribe(()=> {
+    console.log(store.getState())
+})
