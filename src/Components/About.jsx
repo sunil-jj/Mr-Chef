6cb54e25561ab24f5509aeb4dbd615646d19f59e
Sunil-JJ <sunilamaranth@gmail.com>
2023-05-31 19:47:31 +0530

    contact done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,15 +4,15 @@ import React from 'react'
 function About() {
   return (
 
-    <Box bg="aqua">
+    <Box >
       <h2 style={{fontFamily:"cursive",fontWeight:"bold",fontSize:"35px"}} > About Us</h2>
       <Center>
       <Image w="50%"  src=" https://media.istockphoto.com/id/1394055240/photo/happy-black-female-chef-preparing-food-in-frying-pan-at-restaurant-kitchen.jpg?b=1&s=612x612&w=0&k=20&c=orfnpNSuRtQ88QcXjT2_YORJ0DsrLL7uBoE9Pt3njKM="/>
       
       </Center>
         <Center>   
-      <Box borderBottomLeftRadius="70px"  w="50%" bg="purple" c="white">
-          <Container marginLeft="50px" justifyContent="center" maxW="550px">              
+      <Box borderBottomLeftRadius="150px"  w="50%" bg="darkcyan"  color="white">
+          <Container  marginLeft="50px" justifyContent="center" maxW="550px">              
             
                "Mr.Chef is a street food with Odessa hospitality.
 We have been making our guests happy since 2015.
--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,9 +1,29 @@
 
 import React from 'react'
 
+const backgroundStyle = {
+  backgroundImage: 'url(https://media.istockphoto.com/id/1199569763/photo/pizza-in-paper-box-on-wooden-background-copyspace-for-text-and-logo-top-view.jpg?s=612x612&w=0&k=20&c=Z1DCD8TbMnPxLF7gxVuYQbs2bvuS47V1AEf-JHzc2k8=)',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  width: '100%',
+  height: '600px',
+  marginTop:"32px",
+  
+};
+
+
+
+
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div style={backgroundStyle}>
+      <div style={{marginTop:"100px"}} >
+      <h2 style={{color:"white",marginRight:"50%"}}> Hello how are you </h2>
+      </div>
+
+      
+   
+    </div>
   )
 }
 
