99cf775f151bc6a22c186316e9d65bfd0112e703
anannyaSaikia <anannyasaikia1998@gmail.com>
2023-06-03 15:28:55 +0530

    added combineReducers

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,10 @@
-import { legacy_createStore } from "redux";
+import { legacy_createStore, combineReducers } from "redux";
 import { reducer } from "./reducer";
+import { reducerReducer } from "./loginRedux/loginReducer";
 
-export const store = legacy_createStore(reducer);
+const rootReducer = combineReducers({ reducer, reducerReducer});
 
-
-
+export const store = legacy_createStore(rootReducer);
 
 store.subscribe(()=> {
     console.log(store.getState())
